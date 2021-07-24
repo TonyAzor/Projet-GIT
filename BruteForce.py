9bfdc846d9ea88a65dd4afeb76a14e68af26bb35
kinbel <theophane.matis@icloud.com>
2021-07-25 00:11:19 +0200

    passage brute force à linux en passant d'un hash sha256 à un crypt sha512

--- BruteForce.py
+++ BruteForce.py
@@ -1,20 +1,16 @@
 import clear
-import hashlib
+from passlib.hash import sha512_crypt as sha512
 
-liste = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','&','%']
+liste = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','&','%','$','@']
 
-def bruteForce(word, length, hashWord):
+def bruteForce(word, salt, length, hashWord):
     if length > 0:
         for letter in liste:
-            
-            print(word+letter)
-            if hashlib.sha256((word+letter).encode()).hexdigest() == hashWord:
+            if sha512.using(salt=salt).hash(word, rounds=5000) == hashWord:
                 print('Le mot de passe est trouvé et est : '+word+letter)
                 return 0
             else:
-                if bruteForce(word+letter,length-1,hashWord) == 0:
+                if bruteForce(word+letter,salt,length-1,hashWord) == 0:
                     return 0
     return 1
 
-
-bruteForce('',4,'9058f79c893a1d7e20b13009b95b9cf3211478a91a47e2ad65bdfedad6fc98f4')
