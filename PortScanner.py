2773e80bd74bcfb2de11ab2756bb7c91ad49c3c0
TonyAzor <rosatony2112@orange.fr>
2021-07-24 22:18:36 +0200

    Passage sur serveur Linux, le programme devient un service sur le serveur avec le fichier de configuration

--- PortScanner.py
+++ PortScanner.py
@@ -1,13 +1,14 @@
-import socket
+import socket, time, os
 from datetime import datetime
-   
+
+path = "/tmp/Scan/"  
 
    
-def scan(args):
+def scan(firstPort, lastPort):
     target = socket.gethostbyname(socket.gethostname()) 
 
-
-    with open("PortScanLog.txt","w") as scanFile:  
+    
+    with open(path+"portScanLogs","w") as scanFile:  
         # Add Banner 
         scanFile.write("-" * 50+'\n')
         scanFile.write("Scanning Target: " + target+'\n')
@@ -17,7 +18,7 @@ def scan(args):
         try:
             
             # will scan ports between 1 to 65,535
-            for port in range(0,int(args)+1):
+            for port in range(firstPort,lastPort+1):
                 if port == 0:
                     continue
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,3 +38,23 @@ def scan(args):
                 scanFile.write("\n Hostname Could Not Be Resolved !!!!")
         except socket.error:
                 scanFile.write("\\ Server not responding !!!!")
+
+def start():
+    while True:
+        time.sleep(5)
+        if os.path.exists(path+"portScanLogs"):
+            continue
+        else:
+            files = os.listdir(path)
+            for File in files:
+                if ".scan" in File:
+                    ports = File[:-5].split("-")
+                    if len(ports) == 1 and ports[0].isnumeric():
+                        if int(ports[0]) > 0 and int(ports[0])<65536:
+                            scan(1,int(ports[0]))
+                    elif len(ports) == 2 and ports[0].isnumeric() and ports[1].isnumeric():
+                        if int(ports[0]) > 0 and int(ports[0])<=int(ports[1]) and int(ports[1])<65536:
+                            scan(int(ports[0]),int(ports[1]))
+                os.unlink(path+File)       
+
+start()
