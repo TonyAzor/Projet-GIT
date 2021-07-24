10c3216ba168dca7bf6006714ec9e48cd83e2f12
TonyAzor <rosatony2112@orange.fr>
2021-07-24 22:12:21 +0200

    Le scan devient une fonction qui prend un port final en paramètre. Sera exécuté dans un thread

--- PortScanner.py
+++ PortScanner.py
@@ -3,35 +3,37 @@ from datetime import datetime
    
 
    
+def scan(args):
+    target = socket.gethostbyname(socket.gethostname()) 
 
-target = socket.gethostbyname(socket.gethostname()) 
 
-
-with open("PortScanLog.txt","w") as scanFile:  
-    # Add Banner 
-    scanFile.write("-" * 50+'\n')
-    scanFile.write("Scanning Target: " + target+'\n')
-    scanFile.write("Scanning started at:" + str(datetime.now())+'\n')
-    scanFile.write("-" * 50+'\n')
-   
-    try:
-        
-        # will scan ports between 1 to 65,535
-        for port in range(1,10):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
+    with open("PortScanLog.txt","w") as scanFile:  
+        # Add Banner 
+        scanFile.write("-" * 50+'\n')
+        scanFile.write("Scanning Target: " + target+'\n')
+        scanFile.write("Scanning started at:" + str(datetime.now())+'\n')
+        scanFile.write("-" * 50+'\n')
+    
+        try:
             
-            # returns an error indicator
-            result = s.connect_ex((target,port))
-            if result ==0:
-                scanFile.write("\nPort {} is open".format(port))
-            else:
-                scanFile.write("\nPort {} is closed".format(port))
-            s.close()
-            
-    except KeyboardInterrupt:
-            scanFile.write("\n Exitting Program !!!!")
-    except socket.gaierror:
-            scanFile.write("\n Hostname Could Not Be Resolved !!!!")
-    except socket.error:
-            scanFile.write("\\ Server not responding !!!!")
+            # will scan ports between 1 to 65,535
+            for port in range(0,int(args)+1):
+                if port == 0:
+                    continue
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.setdefaulttimeout(1)
+                
+                # returns an error indicator
+                result = s.connect_ex((target,port))
+                if result ==0:
+                    scanFile.write("\nPort {} is open".format(port))
+                else:
+                    scanFile.write("\nPort {} is closed".format(port))
+                s.close()
+                
+        except KeyboardInterrupt:
+                scanFile.write("\n Exitting Program !!!!")
+        except socket.gaierror:
+                scanFile.write("\n Hostname Could Not Be Resolved !!!!")
+        except socket.error:
+                scanFile.write("\\ Server not responding !!!!")
