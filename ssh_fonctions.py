72bea3d899d66271381011934c2cd67e574aede2
kinbel <theophane.matis@icloud.com>
2021-07-25 00:53:03 +0200

    Fichier de fonctions en lien avec ssh, ne contient que de grosses fonction avec un début de traitement

---
+++ ssh_fonctions.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import paramiko
+
+SitesID = {'paris':'1003','grenoble':'1004','strasbourg':'1005','rennes':'1006'}
+IDSites = {'1003':'paris','1004':'grenoble','1005':'strasbourg','1006':'rennes'}      
+
+    
+def createUser(client,sudoPass, carac):
+    command = f"sudo -S useradd -g {carac[4]} -c \"{carac[2]} {carac[1]}\" {carac[0]} && sudo chpasswd <<< {carac[0]}:{carac[3]}"
+    
+    print("Executing {}".format( command ))
+    stdin , stdout, stderr = client.exec_command(command)
+    stdin.write(sudoPass+'\n')
+    stdin.flush()
+    print(stdout.read())
+    print( "Errors")
+    print(stderr.read())
+
+def listingUser(client, sites):
+    listUsers = list()
+    stdin , stdout, stderr = client.exec_command("cat /etc/passwd")
+    users=stdout.read().decode().split('\n')[:-1]
+    for user in users:
+        if user.split(':')[3] in sites[0]:
+            listUsers.append(user)
+    return listUsers
+    
+    # Setup sftp connection and transmit this script
+    #sftp = client.open_sftp()
+    #sftp.put('./PortScanner.py', '/tmp/PortScanner.py')
+    #sftp.close()
